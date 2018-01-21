7b3a30ebdb7f94d43a44ea9338d592522b7e92be
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2018-01-21 19:40:12 +0100

    [IMP] update scripts

--- mlToolkit.sh
+++ mlToolkit.sh
@@ -2,11 +2,14 @@
 # System update
 yum update -y
 # Install Git
-yum install git -y
-
-# Fetch dependencies
+yum install git emacs -y
+# Clone repo
+git clone https://github.com/RaulRC/ml-aws.git
+# Fetch data
+mkdir data
+aws s3 cp s3://poc-ml-op/data/terrorism.csv data/terrorism.csv --region eu-west-1
 aws s3 cp s3://poc-ml-op/resources/requirements.txt requirements.txt --region eu-west-1
-
 # Install dependencies
 pip install -r requirements.txt --no-cache-dir
-
+# Execute program
+python ml-aws/terrorAnalysis.py
--- tensorflow.sh
+++ tensorflow.sh
@@ -5,4 +5,7 @@ yum update -y
 yum install git emacs -y
 # Install Tensor
 pip install --upgrade tensorflow
-
+# Clone repo
+git clone https://github.com/RaulRC/ml-aws.git
+# Execute program
+python ml-aws/helloTensor.py
